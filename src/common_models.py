26242aef4e699f07f4fcb52db3beed1d594e7c14
Veronelli <facu2000veronelli@outlook.com>
2024-04-08 01:16:44 +0000

    add connection to postgres

---
+++ src/common_models.py
@@ -0,0 +1,19 @@
+"""
+Common project models
+"""
+
+from typing import Any
+
+
+class SingletonMeta(type):
+    """
+    class Singleton Meta
+    """
+
+    _instances: Any = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
